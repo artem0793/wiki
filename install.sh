81e4f34a056ae6ddf99b076e01a118cd493a14ec
Vilkov Artem <artem.v@team.anyforsoft.com>
2017-02-13 09:49:57 +0200

    Updated profile

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-drush site-install wiki --account-name=admin --site-mail=artem0793@gmail.com --site-name=WiKi -y
+drush site-install wiki --account-name=admin --site-mail=artem0793@gmail.com --site-name=AnyforSoft Wiki -y
 drush uli
